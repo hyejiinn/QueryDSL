7c7864391fe3b518e7b0a90d8a2fbd264fb84eac
hyejin <hyejinbag772@gmail.com>
2022-10-07 14:06:10 +0900

    20221007 기본 문법 조인 - 기본 조인, 세타 조인

--- src/test/java/study/querydesl/QuerydslBasicTest.java
+++ src/test/java/study/querydesl/QuerydslBasicTest.java
@@ -261,4 +261,45 @@ public class QuerydslBasicTest {
         assertThat(teamB.get(team.name)).isEqualTo("teamB");
         assertThat(teamB.get(member.age.avg())).isEqualTo(35);
     }
+
+    /**
+     * 조인 - 기본 조인
+     * :팀 A에 소속된 모든 회원
+     */
+    @Test
+    public void join() {
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(member.team, team) // leftJoin, rightJoin 모두 가능
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        assertThat(result).extracting("username")
+                .containsExactly("member1", "member2");
+    }
+
+    /**
+     * 세타 조인
+     * 회원의 이름이 팀 이름과 같은 회원 조회
+     */
+    @Test
+    public void theta_join() {
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member, team)
+                .where(member.username.eq(team.name))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("teamA", "teamB");
+    }
 }
