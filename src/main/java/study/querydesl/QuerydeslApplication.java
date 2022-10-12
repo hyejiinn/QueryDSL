8b4703745de5bb86089d1b5d0ca2d130825e2c3e
hyejin <phj981231@naver.com>
2022-10-12 13:45:06 +0900

    20221012 실무 활용(순수 JPA와 QueryDSL) - 순수 JPA 리포지토리와 QueryDSL

--- src/main/java/study/querydesl/QuerydeslApplication.java
+++ src/main/java/study/querydesl/QuerydeslApplication.java
@@ -1,7 +1,11 @@
 package study.querydesl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
 public class QuerydeslApplication {
@@ -9,5 +13,11 @@ public class QuerydeslApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QuerydeslApplication.class, args);
 	}
+
+	// 이렇게 스프링 빈으로 등록해도 됨
+//	@Bean
+//	JPAQueryFactory jpaQueryFactory(EntityManager em) {
+//		return new JPAQueryFactory(em);
+//	}
 
 }
